8c63af391943bd5e076af73d7069a304bf65aa47
Madhumitha-Pragalathan <gp.madhumitha@gmail.com>
2017-06-21 11:27:30 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+import sys
+for i in sys.stdin
+	i = n
+number = int(n)
+if(number % 2 ==0):
+  num = str(number)
+  print ("Number "+num+" is even")
+else :
+  num = str(number)
+  print ("Number "+num+" is odd")
