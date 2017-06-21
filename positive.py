76caaa7d4dc68ca25a80cff2ae1371c643d2dfa6
Madhumitha-Pragalathan <gp.madhumitha@gmail.com>
2017-06-21 10:48:06 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,10 @@
+import sys
+for i in sys.stdin :
+	n1 =i
+number = int(n1)
+if(number  == 0):
+  print ("Number is zero")
+elif(number > 0) :
+    print ("Number is positive")
+else :
+  print ("Number is negative")
